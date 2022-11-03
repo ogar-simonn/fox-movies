2505f6eb577477c23ebab315ddd4e158f3fb2433
Ogar Simon <ogarsimon0000@gmail.com>
2022-11-03 15:52:29 +0100

    bug fixes

--- src/components/Navigations.js
+++ src/components/Navigations.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import data from "../context/Data";
 import { Link } from "react-router-dom";
 
 const Navigations = () => {
   const [value, setValue] = useState(0);
-  const [active, setActive] = useState(false);
   return (
     <nav className="navigations">
       <div className="navigations-container">
